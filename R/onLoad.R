78480496bc78384e851d18ae6c8a2d507fcba81d
pachecotaina <pacheco.taina@gmail.com>
2024-05-21 11:11:04 +0200

    back to v.002
    I could not create a new release (v.004)

--- R/onLoad.R
+++ R/onLoad.R
@@ -4,7 +4,7 @@ radarsp_env <- new.env(parent = emptyenv()) # nocov start
 .onLoad <- function(libname, pkgname){
   
   # data release
-  radarsp_env$data_release <- 'v.004'
+  radarsp_env$data_release <- 'v.002'
   
   # time coverage
   radarsp_env$year_first <- c(2019)
